dbe0289edf289259728ce522c628247beefc8ac6
CristiaN <cristiannavarrete@outlook.com>
2016-04-15 20:41:12 -0300

    First commit

---
+++ config.lua
@@ -0,0 +1,22 @@
+--- MQTT ---
+mqtt_broker_ip = "SERVER"     
+mqtt_broker_port = 1883
+mqtt_username = ""
+mqtt_password = ""
+mqtt_client_id = "ESP866-TAG"
+
+--- WIFI ---
+wifi_SSID = "SSID"
+wifi_password = "PASSWORD"
+-- wifi.PHYMODE_B 802.11b, More range, Low Transfer rate, More current draw
+-- wifi.PHYMODE_G 802.11g, Medium range, Medium transfer rate, Medium current draw
+-- wifi.PHYMODE_N 802.11n, Least range, Fast transfer rate, Least current draw 
+wifi_signal_mode = wifi.PHYMODE_N
+-- If the settings below are filled out then the module connects 
+-- using a static ip address which is faster than DHCP and 
+-- better for battery life. Blank "" will use DHCP.
+-- My own tests show around 1-2 seconds with static ip
+-- and 4+ seconds for DHCP
+client_ip=""
+client_netmask=""
+client_gateway=""
---
+++ init.lua
@@ -0,0 +1,16 @@
+dofile("config.lua")
+
+--Connect to the wifi network
+wifi.setmode(wifi.STATION) 
+wifi.setphymode(wifi_signal_mode)
+wifi.sta.config(wifi_SSID, wifi_password) 
+wifi.sta.connect()
+tmr.alarm(1, 5000, 1, function() 
+    if wifi.sta.getip()== nil then
+        print('IP unavaiable, waiting...') 
+    else
+        tmr.stop(1)
+        print('IP is '..wifi.sta.getip())
+        dofile('connect.lua')
+    end
+ end)
